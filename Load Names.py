1c15bcf91fd0e263aa99774b8742a956cf9e033f
SamHarris2020 <sam.career@outlook.com>
2018-11-26 10:17:55 -0800

    Added more names, currently there is a bug in the sci_names function

--- Load Names.py
+++ Load Names.py
@@ -1,16 +1,42 @@
 from random import randint
 
-adj = tuple(open('adjectives.txt', 'r'))
-nouns = tuple(open('nouns.txt', 'r'))
-verbs = tuple(open('verbs.txt', 'r'))
-
 
 def names():
-    #verbPick = verbs[randint(0, 761)].capitalize().splitlines()[0]
+    adj = tuple(open('adjectives.txt', 'r'))
+    nouns = tuple(open('nouns.txt', 'r'))
     adjPick = adj[randint(0, 626)].capitalize().splitlines()[0]
     nounPick = nouns[randint(0, 2453)].capitalize().splitlines()[0]
-    return adjPick+" "+nounPick
+    return adjPick + " " + nounPick
 
 
-for i in range(10):
-    print(names())
+def big_names():
+    adj = tuple(open('adjectivesLong.txt', 'r'))
+    nouns = tuple(open('nounsLong.txt', 'r'))
+    adjPick = adj[randint(0, 28479)].capitalize().splitlines()[0]
+    nounPick = nouns[randint(0, 90963)].capitalize().splitlines()[0]
+    return adjPick + " " + nounPick
+
+
+def sci_names():
+    adj = tuple(open('adjectivesSci.txt', 'r'))
+    nouns = tuple(open('nounsSci.txt', 'r'))
+    adjPick = adj[randint(0, 53)].capitalize().splitlines()[0]
+    nounPick = nouns[randint(0, 57)].capitalize().splitlines()[0]
+    return adjPick + " " + nounPick
+
+
+def prints():  # Prints 10 Names From Each Name Pool Set
+    print("\n" + "Logical Names:")
+    for i in range(10):
+        print(names())
+
+    print("\n" + "Crazy Names:")
+    for i in range(10):
+        print(big_names())
+
+    print("\n"+"Sci-Fi Names:")
+    for i in range(10):
+        print(sci_names)
+
+
+prints()
