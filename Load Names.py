3683fb1903e3ca0b0c1970de2b730c5c58e9585f
SamHarris2020 <sam.career@outlook.com>
2018-11-26 10:31:39 -0800

    Fixed bug in loadnames

--- Load Names.py
+++ Load Names.py
@@ -1,4 +1,5 @@
 from random import randint
+from Tools.scripts.treesync import raw_input
 
 
 def names():
@@ -9,19 +10,19 @@ def names():
     return adjPick + " " + nounPick
 
 
-def big_names():
-    adj = tuple(open('adjectivesLong.txt', 'r'))
-    nouns = tuple(open('nounsLong.txt', 'r'))
-    adjPick = adj[randint(0, 28479)].capitalize().splitlines()[0]
-    nounPick = nouns[randint(0, 90963)].capitalize().splitlines()[0]
-    return adjPick + " " + nounPick
-
-
 def sci_names():
     adj = tuple(open('adjectivesSci.txt', 'r'))
     nouns = tuple(open('nounsSci.txt', 'r'))
     adjPick = adj[randint(0, 53)].capitalize().splitlines()[0]
     nounPick = nouns[randint(0, 57)].capitalize().splitlines()[0]
+    return adjPick + " " + nounPick
+
+
+def big_names():
+    adj = tuple(open('adjectivesLong.txt', 'r'))
+    nouns = tuple(open('nounsLong.txt', 'r'))
+    adjPick = adj[randint(0, 28479)].capitalize().splitlines()[0]
+    nounPick = nouns[randint(0, 90963)].capitalize().splitlines()[0]
     return adjPick + " " + nounPick
 
 
@@ -36,7 +37,10 @@ def prints():  # Prints 10 Names From Each Name Pool Set
 
     print("\n"+"Sci-Fi Names:")
     for i in range(10):
-        print(sci_names)
+        print(sci_names())
+
+    print("\n")
 
 
 prints()
+raw_input("Press Enter to Close")
